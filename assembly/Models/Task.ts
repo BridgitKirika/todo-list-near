e39326e08840f037ccb7a7066cb5056267db91a1
BridgitKirika <kirikabetty@gmail.com>
2022-05-21 15:27:17 +0300

    fisrt commit

---
+++ assembly/Models/Task.ts
@@ -0,0 +1,14 @@
+import { TaskStatus } from "../utils/enums";
+
+@nearBindgen
+export class Task {
+    title: String;
+    status: TaskStatus;
+
+constructor(title: String) {
+    this.title = title;
+    this.status = TaskStatus.PENDING;
+
+}
+}
+
